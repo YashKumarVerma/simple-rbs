9dbef18d563d6d5bfbb88999226ddb866fa3f3e8
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-03-14 01:47:49 +0530

    feat(response): add response templates

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,29 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import { initializeMongoDB } from './services/database/mongoose'
+import { initializeRedis } from './services/database/redis'
+
+import userRoutes from './modules/user/routes'
+
+/** initialize database connections */
+initializeMongoDB()
+initializeRedis()
 
 /**
  * Initialize express application to hook all middlewares
  */
 const app = express()
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+)
+app.use(bodyParser.json())
 app.get('', (req, res) => {
   res.json({ alive: true })
 })
+
+app.use('/users', userRoutes)
 
 export default app

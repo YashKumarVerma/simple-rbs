429c526e619a1ecbb285d4eb52206d7b147a267e
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-03-19 12:26:01 +0530

    test(e2e): add user role checks

---
+++ test/e2e/user.e2e.ts
@@ -0,0 +1,134 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable no-unused-expressions */
+
+import { describe, it, before } from 'mocha'
+import chai from 'chai'
+import chaiHttpHandler from 'chai-http'
+import config from 'config'
+
+import app from '../../src/app'
+import { UserService } from '../../src/modules/user/service'
+import { logger } from '../../src/services/logger/winston'
+
+import UserModel from '../../src/modules/user/model'
+import { AuthController } from '../../src/modules/auth/controller'
+import { ROLE } from '../../src/services/roles/types'
+
+chai.use(chaiHttpHandler)
+const { expect } = chai
+
+/** silent the logs */
+logger.transports.forEach((t: any) => (t.silent = false))
+
+/**
+ * List of users to use in tests
+ */
+const user = [
+  {
+    age: 20,
+    email: 'test_user_1@example.com',
+    firstName: 'test_user 1',
+    lastName: 'user',
+    password: '12345',
+  },
+  {
+    age: 21,
+    email: 'test_user_2@example.com',
+    firstName: 'test_user 2',
+    lastName: 'user',
+    password: '12345',
+  },
+  {
+    age: 22,
+    email: 'test_user_3@example.com',
+    firstName: 'test_user 3',
+    lastName: 'user',
+    password: '12345',
+  },
+  {
+    age: 23,
+    email: 'test_user_4@example.com',
+    firstName: 'test_user 4',
+    lastName: 'user',
+    password: '12345',
+  },
+]
+
+/**
+ * Rights with visitor are to only create their own profile, nothing else.
+ * controller
+ *      .grant(ROLE.VISITOR)
+ *      .createOwn('profile')
+ */
+describe('users', () => {
+  let cookie: string
+
+  before(async () => {
+    await UserModel.deleteMany({})
+    await Promise.all([UserService.create(user[0]), UserService.create(user[1])])
+
+    const { token } = await AuthController.login({
+      email: user[0].email,
+      password: user[0].password,
+    })
+
+    cookie = `${config.get('login_token.cookie')}=${token}`
+  })
+
+  it('should not be allowed to list details of all users', () => {
+    chai
+      .request(app)
+      .get('/user')
+      .set('Cookie', cookie)
+      .end((err, res) => {
+        expect(res.body.error).to.be.true
+        expect(res.body.code).to.be.equal(403)
+      })
+  })
+
+  it('should not be allowed to view detail of other profile', () => {
+    chai
+      .request(app)
+      .get(`/user/${user[1].email}`)
+      .set('Cookie', cookie)
+      .end((err, res) => {
+        expect(res.body.error).to.be.true
+        expect(res.body.code).to.be.equal(403)
+      })
+  })
+
+  it('should be allowed to view detail of own profile', () => {
+    chai
+      .request(app)
+      .get(`/user/${user[0].email}`)
+      .set('Cookie', cookie)
+      .end((err, res) => {
+        expect(res.body.code).to.be.equal(200)
+        expect(res.body.error).not.to.be.true
+        expect(res.body.payload.email).to.be.equal(user[0].email)
+        expect(res.body.payload.role).to.be.equal(ROLE.USER)
+      })
+  })
+
+  it('should not be allowed to delete any other user', () => {
+    chai
+      .request(app)
+      .delete(`/user/${user[1].email}`)
+      .set('Cookie', cookie)
+      .end((err, res) => {
+        expect(res.body.error).to.be.true
+        expect(res.body.code).to.be.equal(403)
+      })
+  })
+
+  it('should be allowed to delete own profile', () => {
+    chai
+      .request(app)
+      .delete(`/user/${user[0].email}`)
+      .set('Cookie', cookie)
+      .end((err, res) => {
+        expect(res.body.error).not.to.be.true
+        expect(res.body.code).to.be.equal(200)
+      })
+  })
+})
